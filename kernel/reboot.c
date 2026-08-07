MoeKernel/android_kernel_motorola_fogos

rksu: import manual hooks commit adapted from https://github.com/sm6375-fogos/android_kernel_motorola_sm6375-clo/commit/2f1952e4c106efd891d80a01c8ec8c2a8285078a
Signed-off-by: fc5b87cf <90097027+rsuntk@users.noreply.github.com>
Signed-off-by: Akari <barryofc11@gmail.com>

kernel/reboot.c
@@ -310,13 +310,20 @@ DEFINE_MUTEX(system_transition_mutex);
  *
  * reboot doesn't sync: do that yourself before calling this.
  */
+
+#ifdef CONFIG_RKSU
+extern int ksu_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void __user **arg);
+#endif
+
 SYSCALL_DEFINE4(reboot, int, magic1, int, magic2, unsigned int, cmd,
 		void __user *, arg)
 {
 	struct pid_namespace *pid_ns = task_active_pid_ns(current);
 	char buffer[256];
 	int ret = 0;
-
+	#ifdef CONFIG_RKSU
+	ksu_handle_sys_reboot(magic1, magic2, cmd, &arg);
+	#endif
 	/* We only trust the superuser with rebooting the system. */
 	if (!ns_capable(pid_ns->user_ns, CAP_SYS_BOOT))
 		return -EPERM;

AyakaUI-Devices/android_kernel_motorola_sm6375

Signed-off-by: Barry <barryofc11@gmail.com>

kernel/reboot.c
@@ -310,13 +310,20 @@ DEFINE_MUTEX(system_transition_mutex);
  *
  * reboot doesn't sync: do that yourself before calling this.
  */
+
+#ifdef CONFIG_KSU
+extern int ksu_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void __user **arg);
+#endif
+
 SYSCALL_DEFINE4(reboot, int, magic1, int, magic2, unsigned int, cmd,
 		void __user *, arg)
 {
 	struct pid_namespace *pid_ns = task_active_pid_ns(current);
 	char buffer[256];
 	int ret = 0;
-
+	#ifdef CONFIG_KSU
+	ksu_handle_sys_reboot(magic1, magic2, cmd, &arg);
+	#endif
 	/* We only trust the superuser with rebooting the system. */
 	if (!ns_capable(pid_ns->user_ns, CAP_SYS_BOOT))
 		return -EPERM;